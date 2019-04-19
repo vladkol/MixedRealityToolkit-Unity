a8f108bae48224222d062fc3abc4724330040f12
Vlad Kolesnikov <vladimir.kolesnikov@hotmail.com>
2019-04-18 23:06:15 -0700

    Signaling UI

---
+++ Assets/MixedRealityToolkit.Extensions/Signaling/Scripts/ISignalNowAuthenticator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Microsoft.SignalNow.Client
+{
+    public interface ISignalNowAuthenticator
+    {
+        event Action<string> Authenticated;
+        event Action<string> AuthenticationFailed;
+
+        string authentcationServiceName { get; }
+
+        string userName { get; set; }
+        string teamName { get; set; }
+        string companyName { get; set; }
+        string deviceId { get; set; }
+        string authenticationToken { get; }
+    }
+}
